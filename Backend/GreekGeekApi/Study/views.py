164c73f1233672b457bda6aaa66a2baeaf53bf45
Sam Bryant <samwbryant@gmail.com>
2024-10-14 20:14:27 -0500

    Rewrite route method

--- Backend/GreekGeekApi/Study/views.py
+++ Backend/GreekGeekApi/Study/views.py
@@ -3,6 +3,26 @@ from rest_framework import permissions, viewsets
 
 from tutorial.quickstart.serializers import GroupSerializer, UserSerializer
 
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Word
+from .serializers import WordSerializer
+
+@api_view(['GET'])
+def get_words(request):
+    words = Word.objects.all()
+    serializer = WordSerializer(words, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def create_word(request):
+    word = request.data
+    serializer = WordSerializer(data=word)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+    
 
 class UserViewSet(viewsets.ModelViewSet):
     """
