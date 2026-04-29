814c93b920318e88e408b9d5c571035fd1358d6f
Sam Bryant <samwbryant@gmail.com>
2026-04-28 20:29:13 -0400

    CICD fix

--- Backend/scripts/deploy.sh
+++ Backend/scripts/deploy.sh
@@ -1,9 +1,14 @@
-#!/bin/bash	
+#!/bin/bash
+set -euo pipefail
+
 cd /home/sammy/GreekGeek/Backend
 source .venv/bin/activate
-pip install -r requirements.txt
 cd GreekGeekApi/
 git pull --no-edit
+cd ..
+pip install -r requirements.txt
+cd GreekGeekApi/
+python3 manage.py collectstatic --noinput --clear
 python3 manage.py migrate --noinput
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
